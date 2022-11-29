ef7674e036e5190cd1aaedd0cb05528aad8f05ea
zekesarosi <zekesarosi@gmail.com>
2022-11-29 12:29:43 -0700

    more updates

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Jacob Springs Particle Control
- This code that controls a walk-in fridge freezer was written for Jacob Springs Farm near Boulder Colorado.
+
+This code that controls a walk-in fridge freezer was written for Jacob Springs Farm near Boulder Colorado.
 
 For my first year engineering projects class my group and I partnered with Jacob Springs Farm to help them implement a walk-in freezer fridge.
 
-The code is implemented using a Particle Argon device. The code takes in five temperatures from sensors scattered around the fridge and freezer. The device determines if it should activate the compressor and the fan to cool the room. The compressor and the fan are controlled using relays, which the argon device can control with it's output pins. 
+The code is implemented using a Particle Argon device. The code takes in four temperatures from sensors scattered around the fridge and freezer. The device determines if it should activate the compressor and the fan to cool the room. The compressor and the fan are controlled using relays, which the argon device can control with it's output pins. 
+
+When preforming cooling logic for both the compressor and the fan the code first checks for if there are overrides present. If there are overrides then the code will make sure the compressor and the fan are adhereing to those. If no overrides are present then the logic will activate the compressor and the 
 
 If you are trying to replicate the project, or have any questions about the code and or the implemenation don't hesitate to reach out. 
 
--- jacobsprings/jacobsprings.ino
+++ jacobsprings/jacobsprings.ino
@@ -18,11 +18,12 @@
 // SYSTEM_THREAD(ENABLED);
 
 
-uint8_t sensor1[8] = { 0x28, 0xA3, 0x0F, 0x49, 0xF6, 0xF5, 0x3C, 0x20 }; //sensors unique address declarations
+uint8_t sensor1[8] = { 0x28, 0xBB, 0xE0, 0x49, 0xF6, 0x96, 0x3C, 0x60 }; //sensors unique address declarations
 uint8_t sensor2[8] = { 0x28, 0x29, 0x99, 0x49, 0xF6, 0x80, 0x3C, 0x67 };
 uint8_t sensor3[8] = { 0x28, 0x58, 0x39, 0x81, 0xE3, 0xD8, 0x3C, 0xA0 };
 uint8_t sensor4[8] = { 0x28, 0xDE, 0xCA, 0x81, 0xE3, 0x59, 0x3C, 0xFB };
-uint8_t sensor5[8] = { 0x28, 0xBB, 0xE0, 0x49, 0xF6, 0x96, 0x3C, 0x60 };
+uint8_t sensor5[8] = { 0x28, 0xA3, 0x0F, 0x49, 0xF6, 0xF5, 0x3C, 0x20 };
+
 
 int freezerSensors[2] = { 0, 1 };
 int fridgeSensors[2] = { 2, 3 };
@@ -130,29 +131,25 @@ void loop() {
     // Serial.print("Device count: ");
     // Serial.println(sensorNum);
     
-    if ( sensorNum == 0 ){
-        
-    }
-    else{
-        Serial.print(" Requesting temperatures..."); 
-        sensors.requestTemperatures(); // ask sensors for temps
-        Serial.println("Temperatures recieved"); 
-        Serial.println("Updating Temperatures");
-        updateAll(); // update and cout all temperature values
     
-    
-        while ( checkError() ) {
-            coolingLoop();
-        }
-    
-        compressorLogic(); // runs the compressor 
-        fanLogic(); // and fan logic
+    Serial.print(" Requesting temperatures..."); 
+    sensors.requestTemperatures(); // ask sensors for temps
+    Serial.println("Temperatures recieved"); 
+    Serial.println("Updating Temperatures");
+    updateAll(); // update and cout all temperature values
 
-    
-        compressorControl(compressorState); // passes the desired state to the control function
-        fanControl(fanState);
+
+    while ( checkError() ) {
+        coolingLoop();
     }
-    
+
+    compressorLogic(); // runs the compressor 
+    // fanLogic(); // and fan logic
+
+
+    compressorControl(compressorState); // passes the desired state to the control function
+    fanControl(fanState);
+
     
     
     // if (!retryRunning && !Particle.connected()) { // if we have not already scheduled a retry and are not connected
@@ -171,18 +168,24 @@ bool checkError(){
     sensors.requestTemperatures(); // update sensors
     updateAll();
     
-    if ( sensors.getDeviceCount() == 0 ){
-        return true;
-    }
-    else if ( temps[0] == -1000 && temps[1] == -1000 ){
-        return true;
-    }
-    else if ( temps[2] == -1000 && temps[3] == -1000 ){
-        return true;
+    if (!compressorOverrideOn && !compressorOverrideOff && !fanOverrideOn && !fanOverrideOff){
+        if ( sensors.getDeviceCount() == 0 ){
+            return true;
+        }
+        else if ( temps[0] == -1000 && temps[1] == -1000 ){
+            return true;
+        }
+        else if ( temps[2] == -1000 && temps[3] == -1000 ){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
     else {
         return false;
     }
+
 }
 
 void coolingLoop(){
@@ -221,40 +224,28 @@ void coolingLoop(){
 void compressorLogic(){
     if ( !compressorOverrideOn && !compressorOverrideOff) { // first checks if any overrides are active
         if ( freezerCooling ){
-            Serial.println("Freezer Cooling");
             freezerCooling = false;
             for (int j = 0; j < 2; j++){
                 if ( temps[j] != -1000 ){
                     if ( temps[j] > (freezerUpperBoundF - ((freezerUpperBoundF - freezerLowerBoundF) * (3.0/4))) ) {
-                        freezerCooling = true;    
+                        freezerCooling = true;
+                        Serial.println("Cooling Loop will continue");
                         break;
                     }
                 }
-                else {
-                    Serial.print("Cannot Preform Cooling Logic Because Sensor ");
-                    Serial.print(j);
-                    Serial.println(" is offline");
-                }
-                
-            }            
+            }
         }
         if ( !freezerCooling ){
             for (int i = 0; i < 2; i++) { // loops through first two temps
-                if ( temps[i] != -1000 ){
-                   if ( temps[i] >= freezerUpperBoundF ){
-                        compressorState = true; // if the temp is greater than the upper bound compressor state is set to true
-                        freezerCooling = true;
-                        break; // breaks if any of the sensors are greater.
-                    }
-                    else {
-                        compressorState = false;
-                    } 
+               if ( temps[i] >= freezerUpperBoundF ){
+                    Serial.println("Cooling loop activated");
+                    compressorState = true; // if the temp is greater than the upper bound compressor state is set to true
+                    freezerCooling = true;
+                    break; // breaks if any of the sensors are greater.
                 }
                 else {
-                    Serial.print("Cannot Preform Cooling Logic Because Sensor ");
-                    Serial.print(i);
-                    Serial.println(" is offline");
-                }
+                    compressorState = false;
+                } 
 
             }
         }
@@ -271,21 +262,12 @@ void compressorLogic(){
 // ignore fan logic 
 void fanLogic(){
     if ( !fanOverrideOn && !fanOverrideOff) { // first checks if any overrides are active
-<<<<<<< HEAD
         if ( fridgeCooling ){
             Serial.println("Fridge Cooling");
             freezerCooling = false;
-            for (int j = 0; j < 2; j++){
+            for (int j = 2; j < 4; j++){
                 if ( temps[j] > (fridgeUpperBoundF - ((fridgeUpperBoundF - fridgeLowerBoundF) * (3.0/4))) ) {
                     fridgeCooling = true;    
-=======
-        for (int i = 2; i < 4; i++) { // loops temps 2-3
-            if ( fridgeCooling ) {
-                if ( temps[i] > (fridgeUpperBoundF - ((fridgeUpperBoundF - fridgeLowerBoundF) * (3.0/4))) ) {
-<<<<<<< Updated upstream
-=======
->>>>>>> b2d6f6b3f8fa71aef5553ede069e63ac7d012601
->>>>>>> Stashed changes
                     break;
                 }
             }            
@@ -347,8 +329,8 @@ void updateAll() {   //updates all 5 sensors
     Serial.print("Sensor 4: ");
     temps[3] = updateTemperature(sensor4);
     
-    // Serial.print("Sensor 5: ");
-    // temps[4] = updateTemperature(sensor5);
+    Serial.print("Sensor 5: ");
+    temps[4] = updateTemperature(sensor5);
 }
 
 
@@ -396,7 +378,7 @@ int fanOverrideToOff(String command){
 
 
 
-int resetOverrides(String command){ //reset overrides to no override
+int resetOverrides(String command){ //reset overrides to default no override
     fanOverrideOn = false;
     compressorOverrideOn = false;
     fanOverrideOff = false;
@@ -404,7 +386,7 @@ int resetOverrides(String command){ //reset overrides to no override
     return 1;
 }
 
-int resetCustomTemps(String command){ //reset custom temps to the default
+int resetCustomTemps(String command){ //reset custom temps to the default temps
     freezerUpperBoundF = 0;
     freezerLowerBoundF = -8;
     fridgeUpperBoundF = 38;
@@ -414,11 +396,11 @@ int resetCustomTemps(String command){ //reset custom temps to the default
 
 
 int freezerBoundUpperF(String command){
-    freezerUpperBoundF = command.toFloat(); // takes the funcion input convers to float and then truncates to int for temperature bounds
+    freezerUpperBoundF = command.toFloat(); // takes the funcion input converts to float and then truncates to int for temperature bounds
     return freezerUpperBoundF;
 }
 int freezerBoundLowerF(String command){
-    freezerLowerBoundF = command.toFloat(); 
+    freezerLowerBoundF = command.toFloat(); //mostly the same as above 
     return freezerLowerBoundF;
 }
 
@@ -431,28 +413,5 @@ int fridgeBoundLowerF(String command){
     return fridgeLowerBoundF;
 }
 
-// void retryConnect()
-// {
-//   if (!Particle.connected())   // if not connected to cloud
-//   {
-//     Serial.println("reconnect");
-//     stopTimer.start();         // set of the timout time
-//     WiFi.on();
-//     Particle.connect();        // start a reconnectino attempt
-//   }
-//   else                         // if already connected
-//   {
-//     Serial.println("connected");
-//     retryTimer.stop();         // no further attempts required
-//     retryRunning = false;
-//   }
-// }
 
-// void stopConnect()
-// {
-//     Serial.println("stopped");
-
-//     if (!Particle.connected()) // if after retryTime no connection
-//       WiFi.off();              // stop trying and swith off WiFi
-//     stopTimer.stop();
-// }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
